71c65f83139c580e0d57aa3a8818b423c33741c7
Dani-Marquez <dani.marquez23@outlook.com>
2018-07-03 12:27:54 -0500

    Nueva

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+package esturcturadatos;
+
+public class HolaMundo {
+
+    public static void main(String [] args) {
+     System.out.println("Hola Mundo");
+    new java.util.Scanner(System.in).nextLine();
